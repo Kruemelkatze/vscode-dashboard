aca42b0f587d3e50eddd44d00a277c57fead9036
Fabian Schober <schoberf@gmx.net>
2018-08-25 23:56:50 +0200

    Generate random id for projects

--- src/models.ts
+++ src/models.ts
@@ -20,8 +20,18 @@ export class Project {
     imageFilePath: string;
 
     constructor(name: string, path: string) {
-        this.id = name.toLowerCase().replace(/[^a-z0-9]/, '');
+        this.id = generateRandomId(name);
         this.name = name;
         this.path = path;
     }
+}
+
+function generateRandomId(prepend: string = null) {
+    if (prepend) {
+        prepend = prepend.toLowerCase().replace(/[^a-z0-9]/, '').substring(0, 24);
+    } else {
+        prepend = '';
+    }
+
+    return Math.random().toString(36).substr(2, 9) + Date.now().toString(36);
 }
