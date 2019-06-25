7f38d55f1bdc70022f02e54a55cc6b2fd1f602ee
Fabian Schober <schoberf@gmx.net>
2019-06-25 22:14:41 +0200

    Edit & Delete Project buttons (yet without function)

--- src/webviewContent.ts
+++ src/webviewContent.ts
@@ -66,6 +66,12 @@ function getProjectDiv(project: Project) {
 <div class="project-container">
     <div class="project" data-id="${project.id}" 
          style="${project.color ? `border-top-color: ${project.color};` : ''}">
+        <div class="project-actions-wrapper">
+            <div class="project-actions">
+                <span data-action="edit">${getEditIcon()}</span>
+                <span data-action="delete">${getDeleteIcon()}</span>
+            </div>
+        </div>
         <div class="fitty-container">
             <h2 class="project-header">
                 ${USE_PROJECT_ICONS && project.imageFileName ? `<img src="${getImagePath(project)}" />` : ''}
@@ -174,6 +180,23 @@ function onAddProjectClicked(e) {
     });
 }
 
+function onTriggerProjectAction(target, projectId) {
+    var actionDiv = target.closest('[data-action]')
+    if (actionDiv == null)
+        return false;
+
+    var action = actionDiv.getAttribute("data-action");
+    if (!action)
+        return false;
+
+    vscode.postMessage({
+        type: action + '-project',
+        projectId,
+    });
+
+    return true;
+}
+
 document.addEventListener('click', function(e) {
     if (!e.target)
         return;
@@ -182,12 +205,14 @@ document.addEventListener('click', function(e) {
     if (!projectDiv)
         return;
     
-    var dataId =projectDiv.getAttribute("data-id");
+    var dataId = projectDiv.getAttribute("data-id");
     if (dataId == null)
         return;
 
-    var newWindow = !!e.ctrlKey;
+    if (onTriggerProjectAction(e.target, dataId))
+        return;
 
+    var newWindow = !!e.ctrlKey;
     onProjectClicked(dataId, newWindow);
 });
 
@@ -226,7 +251,7 @@ window.onload = () => {
         }
 
         vscode.postMessage({
-            type: 'projects-reordered',
+            type: 'reordered-projects',
             groupOrders,
         });
     }
@@ -247,6 +272,22 @@ function getGitSvgIcon() {
         C95.021,50.468,95.021,46.719,92.71,44.408z"/>
 </svg>
     `;
+}
+
+function getEditIcon() {
+    return `
+<svg viewBox="0 0 512 512">
+    <path d="M290.74 93.24l128.02 128.02-277.99 277.99-114.14 12.6C11.35 513.54-1.56 500.62.14 485.34l12.7-114.22 277.9-277.88zm207.2-19.06l-60.11-60.11c-18.75-18.75-49.16-18.75-67.91 0l-56.55 56.55 128.02 128.02 56.55-56.55c18.75-18.76 18.75-49.16 0-67.91z"/>
+</svg>
+`;
+}
+
+function getDeleteIcon() {
+    return `
+<svg viewBox="0 0 512 512">
+    <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"/>
+</svg>
+`;
 }
 
 function getMediaResource(context: vscode.ExtensionContext, name: string) {
