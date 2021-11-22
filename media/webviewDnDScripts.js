c24efd36e089ea48187ef15f2f8bb230d4a84f2b
Kruemelkatze <schoberf@gmx.net>
2021-11-22 23:24:14 +0100

    Fixed undefined access in onReordered

--- media/webviewDnDScripts.js
+++ media/webviewDnDScripts.js
@@ -25,13 +25,11 @@ function initDnD() {
     function onReordered() {
         // Build reordering object
         let groupElements = [...document.querySelectorAll(`${groupsContainerSelector} > [data-group-id]`)];
-        // If showAddGroupButtonTile setting is true & a project was dropped on the Create New Group element...
-        if (infos.config.showAddGroupButtonTile) {
-            let tempGroupElement = document.querySelector('#tempGroup');
-            if (tempGroupElement && tempGroupElement.querySelector("[data-id]")) {
-                // ... Handle it as a new group
-                groupElements.push(tempGroupElement);
-            }
+        // If a project was dropped on the Create New Group element...
+        let tempGroupElement = document.querySelector('#tempGroup');
+        if (tempGroupElement && tempGroupElement.querySelector("[data-id]")) {
+            // ... Handle it as a new group
+            groupElements.push(tempGroupElement);
         }
 
         let groupOrders = [];
